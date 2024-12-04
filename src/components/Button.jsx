e0e812bff5beb93718da329944322956e408fe52
AliHosseini370 <100512142+AliHosseini370@users.noreply.github.com>
2024-12-04 17:44:23 +0330

    Delete src directory

--- src/components/Button.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Button = ({ styles }) => (
-    <button type='button' className={`py-4 px-6 button_white_gradient font-medium text-[18px] text-primary outline-none ${styles} rounded-[10px]`}>
-      همین حالا تماس بگیر
-    </button>
-)
-
-export default Button
